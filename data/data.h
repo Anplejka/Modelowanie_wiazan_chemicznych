041559a5baf1efc70f670b5dcb22563ce4e3c962
kasia <katarzyna.harlak@student.uj.edu.pl>
2026-05-18 21:25:22 +0200

    <3

--- data/data.h
+++ data/data.h
@@ -4,5 +4,15 @@
 
 #ifndef MODELOWANIE_WIAZAN_CHEMICZNYCH_DATA_H
 #define MODELOWANIE_WIAZAN_CHEMICZNYCH_DATA_H
-
+typedef struct {
+    float x;
+    float y;
+    float z;
+} position_t;
+typedef struct {
+    int id;
+    int type;
+    position_t pos;
+} atom_t;
+int read_at
 #endif //MODELOWANIE_WIAZAN_CHEMICZNYCH_DATA_H

d046761eec456bc0ba5d45c55aad99195570197f
laura <laura.rysz@student.uj.edu.pl>
2026-05-18 21:47:58 +0200

    update4

--- chemistry/chemistry.h
+++ chemistry/chemistry.h
@@ -5,6 +5,6 @@
 #ifndef MODELOWANIE_WIAZAN_CHEMICZNYCH_CHEMISTRY_H
 #define MODELOWANIE_WIAZAN_CHEMICZNYCH_CHEMISTRY_H
 float distance(atom_t a, atom_t b);
-float angle(position_t a, position_t b);
+float angle(atom_t a, atom_t b);
 
 #endif //MODELOWANIE_WIAZAN_CHEMICZNYCH_CHEMISTRY_H

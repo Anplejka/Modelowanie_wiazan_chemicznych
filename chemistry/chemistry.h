6070c4481d685e3c7fcc2ce3d6098b11f18fb70f
Anna Kowalska <anna.2.kowalska@student.uj.edu.pl>
2026-05-18 21:51:50 +0200

    include data do chemistry.h

--- chemistry/chemistry.h
+++ chemistry/chemistry.h
@@ -4,6 +4,8 @@
 
 #ifndef MODELOWANIE_WIAZAN_CHEMICZNYCH_CHEMISTRY_H
 #define MODELOWANIE_WIAZAN_CHEMICZNYCH_CHEMISTRY_H
+#include "../data/data.h"
+
 float distance(atom_t a, atom_t b);
 float angle(atom_t a, atom_t b);
 
